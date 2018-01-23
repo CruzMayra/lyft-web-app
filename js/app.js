e13e7c6c584865b79abac246ed4c69eb938cac64
CruzMayra <velazquez.leonora@gmail.com>
2018-01-23 00:47:02 -0600

    creación de funciones para validar data del usuario

--- js/app.js
+++ js/app.js
@@ -3,48 +3,84 @@ var longitudeCode = 3;
 
 var $phoneNumberInput = $('#phone-number');
 var $linkToVerifyView = $('.verify-phone-next');
+var $codeVerifyInput = $('#verify-code');
+var $agreeTermsUser = $('#grid-check');
 
 $(document).ready(loadPage);
 
-function loadPage() { // función que centraliza al resto de las funciones
+ // función que centraliza al resto de las funciones
+function loadPage() {
   loadSplashView();
   loadMainView();
   $phoneNumberInput.keyup(validatePhoneNumber);
   $linkToVerifyView.click(codeGenerator);
+  $codeVerifyInput.keyup(validateCode);
+  $agreeTermsUser.click(validateUserData);
 }
 
-function loadSplashView() { // función para mostrar la vista splash por tres segundos
+// función para mostrar la vista splash por tres segundos
+function loadSplashView() {
   setTimeout(function() {
     $("#view-splash").fadeOut(1500);
   },3000);
 }
 
-function loadMainView() { // función que muestra la vista principal pasado la vista splash
+// función que muestra la vista principal pasado la vista splash
+function loadMainView() {
   setTimeout(function() {
     $("#signup").fadeIn(1500);
   },3000);
 }
 
+// función que valida el número telefonico ingresado por el usuario para habilitar botón
 function validatePhoneNumber() {
-  var $buttonToVerifyView = $('.verify-phone');
-
-  if($(this).val().trim().length === 10) {
-    $buttonToVerifyView.removeAttr('disabled');
-    // $linkToVerifyView.attr("href", "verify-view.html");
+  if($('#phone-number').val().trim().length === 10) {
+    $('.verify-phone').removeAttr('disabled');
+    return true
   }else {
-    $buttonToVerifyView.attr('disabled', true);
-    // $linkToVerifyView.attr('href','#');
+    $('.verify-phone').attr('disabled', true);
+    return false
   }
 }
 
+/* función para generar código aleatorio */
 function codeGenerator(e){
   e.preventDefault();
-  code = "";
+  //validando input del phone number para prevenir compartamiendo del enlace
+  var isValid = validatePhoneNumber();
+  if( !isValid) {
+    return
+  }
+  var code = "";
   for(var i = 0; i < longitudeCode; i++) {
     randomNumber = Math.floor(Math.random() * digits.length);
     code += digits.substr(randomNumber, 1);
   }
-  // $linkToVerifyView.attr("href", "verify-view.html");
-  alert('Your code is: LAB-' + code);
+  code = 'LAB-' + code;
+  localStorage.setItem('labCode', code);
+  alert('Your code is: ' + code);
   window.location.href = "verify-view.html";
+}
+
+//función que valida el código enviado al usuario y permite la redirección a la siguiente vista
+function validateCode(){
+  var codeSent = localStorage.getItem('labCode');
+   if($(this).val().trim() === codeSent) {
+   $('.verify-code-next').removeAttr('disabled');
+   $('.to-agree-view').attr('href','agree-view.html');
+ }else {
+   $('.verify-code-next').attr('disabled', true);
+   $('.to-agree-view').attr('href','#');
+ }
+}
+
+function validateUserData(){
+  if($('.first-name').val().trim().length > 3 &&
+     $('.second-name').val().trim().length > 3) {
+    $('.finish-next').removeAttr('disabled');
+    $('.to-finish-view').attr('href','finish-view.html');
+  } else {
+    $('.finish-next').attr('disabled', true);
+    $('.to-finish-view').attr('href','#');
+  }
 }
